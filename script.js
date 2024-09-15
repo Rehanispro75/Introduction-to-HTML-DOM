418080e5b9e33de6cbeb5a8c4cfb350e3f3c1e90
Rehanispro75 <rehanshaikh218053@gmail.com>
2024-09-15 19:58:28 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+
+
+
+function callAlert(){
+
+    alert("you are calling alert function")
+}
+function validate
+e.preventDefault();
+var email = document.getElementById("email").value
+if (email === "")
+
+    document.getElementById("meassage").innerHTML="email cannot be empty"
+    else {
+
+        document.getElementById("message").innerHTML="login successfully"
+        msgBox.innerText=message
+    }
